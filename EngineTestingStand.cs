aed03efe98e30fe20c93b9e5184e254d5926a7a8
d4nnoyer <rubansky.pavel@gmail.com>
2021-07-30 12:26:32 +0700

    Initial Commit

---
+++ EngineTestingStand.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace DvsTesting
+{
+    public static class EngineTestingStand
+    {
+        
+        private static int _lastTestDuration;
+        
+        public static void StartNewTest(Engine engine, double envTemperature)
+        {
+            _lastTestDuration = 0;
+            engine.Reset(envTemperature);
+            while (!engine.IsOverheat)
+            {
+                engine.Work(envTemperature);
+                _lastTestDuration++;
+            }
+        }
+        
+
+        public static string LastTestDuration 
+            => _lastTestDuration.ToString(CultureInfo.CurrentCulture);
+    }
+}
